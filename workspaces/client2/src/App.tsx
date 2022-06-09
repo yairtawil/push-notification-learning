5c358e72f450a9414214a7d7056d6c8ada322d43
YairTawil <yairtawil@gmail.com>
2022-06-09 14:32:17 +0300

    what

---
+++ workspaces/client2/src/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+
+
+
+function App() {
+  return (
+    <div className="App">
+      <button>WOWWWWWWWWWWWWWWWWWWWW</button>
+    </div>
+  );
+}
+
+export default App;
